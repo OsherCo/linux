758b65c7063bfcc39318af921fd42a9102aad51b
OsherCo <osher1598@gmail.com>
2021-09-12 18:03:36 +0300

    added kernel modules again

---
+++ tools/labs/skels/kernel_modules/1-2-test-mod/.modules.order.cmd
@@ -0,0 +1,1 @@
+cmd_/root/linux/tools/labs/skels/./kernel_modules/1-2-test-mod/modules.order := {   echo /root/linux/tools/labs/skels/./kernel_modules/1-2-test-mod/hello_mod.ko; :; } | awk '!x[$$0]++' - > /root/linux/tools/labs/skels/./kernel_modules/1-2-test-mod/modules.order
